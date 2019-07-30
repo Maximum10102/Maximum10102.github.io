9326718b18ae59e6911e00bb9dacf4987b728dfe
Maximum10102 <48498503+Maximum10102@users.noreply.github.com>
2019-07-30 16:54:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let gif = document.getElementById('g')
 
 if(document.clientWidth <= 1280 || document.clientHeight <= 576){
-    gif.outerHTML == "style='max-width: 500;'"
+    gif.outerHTML ='<img src="/Computer.gif" style="max-width: 500;" alt="Old computer  gif" id="g"></img>'
 }
